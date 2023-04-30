093fa0a73d6b79d12382b179d6f49b91f1d93399
Dangeonman <svfox070@gmail.com>
2023-04-30 18:18:43 +0300

    ready

--- test.py
+++ test.py
@@ -1,26 +1,41 @@
 import os.path
 import xmltodict
 import urllib.request
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--list", action="store_true", help = "Вывод доступных валют")
 
 URL_EXCHANGE_VAL_LIST = "http://www.cbr.ru/scripts/XML_val.asp?d=0"
 
 
-path = './list.xml'
 
-def getDictFromUrl(URL):
-    file = urllib.request.urlopen(URL)
-    data =file.read()
-    file.close()
-    data =xmltodict.parse(data)
-    return data
+file = urllib.request.urlopen(URL_EXCHANGE_VAL_LIST)
+data =file.read()
+file.close()
+    
+with open("list.xml", "w", encoding="utf-8") as out:
+    out.write(str(data))
 
-list_file = os.path.isfile(path)
+def Name_Valuta():
+    data =open("list.xml", "r", encoding="utf-8")
+    new_data = data['Valuta']['Item']
+    try:
+        for i in new_data:
+            print("Валюта:", i['Name'], "Код:", i['@ID'])
+    except:
+        str1 = "Ошибка"
+        print(str1)
+        quit(-1)
 
-if list_file == True:
-    print("Такой файл существует")
+argument = parser.parse_args()
+
+if argument.list:
+    Name_Valuta()
 else:
-    list_file = open("list.xml", "w", encoding="utf-8")
-    list_file.write(str(getDictFromUrl(URL_EXCHANGE_VAL_LIST)))
-    print(list_file)
+    print("Неизвестная команда")
+
+
+
 
